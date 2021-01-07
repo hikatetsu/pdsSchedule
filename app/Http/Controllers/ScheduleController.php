a6ce7e6a7007360dc721a9f1638670718322a0ad
tetsu <cda07c5117@gmail.com>
2021-01-07 16:05:24 +0900

    スケジュール作成ページ準備

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -9,4 +9,8 @@ class ScheduleController extends Controller
     public function index(){
         return view('schedules.index');
     }
+
+    public function create(){
+        return view('schedules.create');
+    }
 }
